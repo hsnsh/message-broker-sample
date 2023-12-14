2adeee7e1e30190b05eb839e0b89d8b19c5fbbc2
Hasan H. SAHIN <hasansahin@araskargo.com.tr>
2023-12-14 17:48:48 +0300

    ok

---
+++ Shared/EmailMessage.cs
@@ -0,0 +1,11 @@
+﻿using Shared;
+
+namespace Kafka.Message
+{
+    public class EmailMessage : IIntegrationEvent
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Shared/IIntegrationEvent.cs
@@ -0,0 +1,5 @@
+namespace Shared;
+
+public interface IIntegrationEvent
+{
+}
