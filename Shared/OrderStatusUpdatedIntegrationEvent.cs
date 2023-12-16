eeb0ba29f5abffb29c3be25f08d7d7dc0cb8dea3
Hasan H. SAHIN <hasansahin@araskargo.com.tr>
2023-12-17 02:58:39 +0300

    ok

--- Shared/OrderStatusUpdatedIntegrationEvent.cs
+++
@@ -1,8 +0,0 @@
-using Base.EventBus;
-
-namespace Shared;
-
-public record OrderStatusUpdatedIntegrationEvent(Guid Id, DateTime CreationTime, Guid OrderId) : IntegrationEvent(Id, CreationTime)
-{
-    public Guid OrderId { get; } = OrderId;
-}
