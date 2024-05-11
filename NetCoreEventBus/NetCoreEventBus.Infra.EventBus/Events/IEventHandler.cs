63a15e976692c8b4fb88dd5695d5306bf2d7cd6e
Hasan H. SAHIN <hasansahin@araskargo.com.tr>
2024-05-11 18:31:50 +0300

    ok

--- NetCoreEventBus/NetCoreEventBus.Infra.EventBus/Events/IEventHandler.cs
+++ NetCoreEventBus/NetCoreEventBus.Infra.EventBus/Events/IEventHandler.cs
@@ -1,9 +1,5 @@
 ﻿namespace NetCoreEventBus.Infra.EventBus.Events;
 
-/// <summary>
-/// Contract for event handlers. Event handlers are responsible for processing events when they happen.
-/// </summary>
-/// <typeparam name="TEvent">Event type.</typeparam>
 public interface IEventHandler<in TEvent>
 	where TEvent : Event
 {
